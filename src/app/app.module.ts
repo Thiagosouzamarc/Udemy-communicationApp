442229bcd5f9b2c6b30d646204464d7c8d515f95
Thiago Souza Marciano <thiagomarciano@brq.com>
2019-09-06 19:03:19 -0300

    06-09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { InputBindingComponent } from './input-binding/input-binding.component';
 import { ClientComponent } from './input-binding/client/client.component';
-
+import { EventComponent } from './event/event.component';
+import { ChildItemComponent } from './event/child-item/child-item.component';
+import { MatCardModule, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+import { ClientsComponent } from './clients/clients.component';
+import { FormsModule } from '@angular/forms';
+import { ItemClientComponent } from './clients/item-client/item-client.component';
 @NgModule({
   declarations: [
     AppComponent,
     InputBindingComponent,
-    ClientComponent
+    ClientComponent,
+    EventComponent,
+    ChildItemComponent,
+    ClientsComponent,
+    ItemClientComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
